45a4f685b0e5dcf1681be3459ef18c1f864a4d90
Sujan2332 <saisujan.s03@gmail.com>
2025-01-21 00:03:46 +0530

    chnages

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -10,10 +10,10 @@ const UserSchema = new mongoose.Schema({
     resetToken: { type: String, required: false }, // Add this field
 },{collection:"quizusers"});
 
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
+// UserSchema.pre('save', async function (next) {
+//     if (!this.isModified("password")) return next();
+//     this.password = await bcrypt.hash(this.password, 10);
+//     next();
+// });
 
 module.exports = mongoose.model("User", UserSchema);
